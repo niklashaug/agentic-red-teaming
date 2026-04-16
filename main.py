93d793ec449c5b2c9daad1ba83e1223d81cd124e
Niklas Haug <niklas.haug@hs-augsburg.de>
2026-04-16 07:40:41 +0200

    add ollama

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+from ollama import chat
+from ollama import ChatResponse
+
+
 def main():
     print("Hello from agentic-red-teaming!")
+    response: ChatResponse = chat(
+        model="gemma3",
+        messages=[
+            {
+                "role": "user",
+                "content": "Why is the sky blue?",
+            },
+        ],
+    )
+    print(response["message"]["content"])
+    # or access fields directly from the response object
+    print(response.message.content)
 
 
 if __name__ == "__main__":
